6c1fc0969e7a26a653faed3ace86f0ad321f847a
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-01-23 12:41:26 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Jonathan Wheeler Jr. on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
