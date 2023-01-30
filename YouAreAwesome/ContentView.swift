a298cf8c18158d13f59a36a672fab9087bc2b445
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-01-30 10:54:02 -0500

    1.7 Completed

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,51 +11,51 @@ struct ContentView: View {
     @State private var messageString = "When the Genius Bar needs help, they call you!"
     var body: some View {
         
-        ZStack {
-            Rectangle()
-                .fill(Gradient(colors: [.white, .blue, .red]))
-                .ignoresSafeArea()
-            
-            VStack (spacing: 0){
-                Text("Your Have Skills!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(Color("Gold-BC"))
-                    .padding()
-                    .background(Color("Maroon-BC"))
-                    .cornerRadius(15)
-                 
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                
-                Spacer()
-                
-                HStack {
-                    Button("Awesome") {
-                        // This is the action performed when the button is pressed
-                        messageString = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                .padding()
+        VStack {
+            Group {
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
             }
+            .font(.title)
+            .fontWeight(.heavy)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            
+            HStack {
+                Button("Awesome") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
         }
     }
 }
