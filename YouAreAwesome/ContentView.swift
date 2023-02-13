d1339ede842f15ae91a9f83157886a742a1d32e3
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-02-13 11:03:42 -0500

    1.22 Completed

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var lastMessageNumber = -1
     @State private var lastSoundNumber = -1
     @State private var audioPlayer: AVAudioPlayer!
+    @State private var soundIsOn = true
     
     var body: some View {
         
@@ -41,22 +42,39 @@ struct ContentView: View {
             
             Spacer()
             
-            Button("Show Message") {
-                let messages = ["You are Awesome",
-                                "You are Great",
-                                "You are Fantastic",
-                                "Fabulous? That's You!",
-                                "You Make Me Smile",
-                                "When the Genius Bar Needs Help, They Call You!"]
-                lastMessageNumber = nonRepeatingNumber(lastNumber: lastMessageNumber, upperBound: messages.count-1)
-                messageString = messages[lastMessageNumber]
-                lastImageNumber = nonRepeatingNumber(lastNumber: lastImageNumber, upperBound: 9)
-                imageName = "image\(lastImageNumber)"
+            HStack {
                 
-                lastSoundNumber = nonRepeatingNumber(lastNumber: lastSoundNumber, upperBound: 5)
-                playSound(soundName: "sound\(lastSoundNumber)" )
+                Text("Sound On:")
+                Toggle("", isOn: $soundIsOn)
+                    .labelsHidden()
+                    .onChange(of: soundIsOn) {_ in
+                        if audioPlayer != nil && audioPlayer.isPlaying {
+                            audioPlayer.stop()
+                        }
+                    }
+                
+                Spacer()
+                
+                Button("Show Message") {
+                    let messages = ["You are Awesome",
+                                    "You are Great",
+                                    "You are Fantastic",
+                                    "Fabulous? That's You!",
+                                    "You Make Me Smile",
+                                    "When the Genius Bar Needs Help, They Call You!"]
+                    lastMessageNumber = nonRepeatingNumber(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                    messageString = messages[lastMessageNumber]
+                    lastImageNumber = nonRepeatingNumber(lastNumber: lastImageNumber, upperBound: 9)
+                    imageName = "image\(lastImageNumber)"
+                    
+                    lastSoundNumber = nonRepeatingNumber(lastNumber: lastSoundNumber, upperBound: 5)
+                    if soundIsOn {
+                        playSound(soundName: "sound\(lastSoundNumber)" )
+                        
+                    }
+                }
+                .buttonStyle(.borderedProminent)
             }
-            .buttonStyle(.borderedProminent)
             
         }
         .padding()
